12bd2de781b5f0fe3f096eec6c3426de19bb5a40
marian <mariangonzalezcalderon@gmail.com>
2026-04-27 00:37:44 -0300

    fix pkg_resources definitivo

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+pip install --upgrade pip
+
+# 👇 CLAVE: instala pkg_resources correctamente
+pip install "setuptools==68.0.0"
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
